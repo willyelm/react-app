6f7eeec0b339f9d25aa31d1f0794210bc829fb7b
Will Medina <williams.medinaa@gmail.com>
2025-01-23 05:32:16 -0700

    chore: simplify Home layout

--- src/App/Home.tsx
+++ src/App/Home.tsx
@@ -12,25 +12,30 @@ export const Home: FunctionComponent = () => {
     <title> App </title>
     <meta name='description' content='This is my home page'/>
 
-    <div className='flex flex-col w-full min-h-screen border-x border-gray-700 divide-y divide-gray-700 max-w-3xl mx-auto'>
-      <div className="flex divide-x divide-gray-700 p-8">
-        <h1 className='text-4xl'> App </h1>
-      </div>
-      <div className="flex flex-col divide-gray-700 p-8">
-        <h2 className='mb-4'>
-          Counter: {count}
-        </h2>
+    <header>
+      <nav className='flex justify-center items-center p-8'>
+        <h1>App</h1>
+      </nav>
+    </header>
+
+    <div className='flex flex-col w-full max-w-3xs mx-auto my-32 border border-gray-700'>
+      <div className="flex-auto flex flex-col items-center p-8 space-y-4">
+        <h1 className='mb-4'>
+          Counter
+        </h1>
+        <p className='text-4xl'>
+          {count}
+        </p>
         <button className='px-4 py-2 bg-white text-black' onClick={() => update()}>
           Increase
         </button>
       </div>
-      <div className="flex-auto flex divide-x divide-gray-700 min-h-16 p-8">
-        <footer className='flex items-center space-x-4 px-8 py-4'>
-          <span className='italic text-gray-500'>Built with </span>
-          <React width={16} height={16}/>
-          <ESbuild width={16} height={16}/>
-        </footer>
-      </div>
     </div>
+
+    <footer className='flex justify-center items-center space-x-4 px-8 py-4'>
+      <span className='italic text-gray-500'>Built with </span>
+      <React width={16} height={16}/>
+      <ESbuild width={16} height={16}/>
+    </footer>
   </>;
 }
